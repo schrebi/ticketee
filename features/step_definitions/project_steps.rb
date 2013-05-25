8151c4bdc126024df56bb7547882e577ecacac02
schrebi <schrebi@gmail.com>
2013-05-25 21:02:25 +0200

    Added updating projects functionality

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -9,3 +9,15 @@ end
 Then(/^I should be on the project page for "(.*?)"$/) do |name|
   page.should have_content(name)
 end
+
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
+ fill_in(arg1, :with => arg2)
+end
+
+When(/^I press "(.*?)"$/) do |arg1|
+  click_button(arg1)
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  page.should have_content(arg1)
+end
